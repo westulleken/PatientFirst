c80ec53bb117db74c3a746a79be9430d628a1ef6
westulleken <78081982+westulleken@users.noreply.github.com>
2025-11-29 09:19:41 +0200

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask_cors import CORS
+from routes.patient_routes import patient_bp
 
 app = Flask(__name__)
 CORS(app)
+
+app.register_blueprint(patient_bp, url_prefix="/api")
 
 @app.route("/")
 def home():
