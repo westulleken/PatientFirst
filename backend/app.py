60fa58ddc32800151e7f5e37698eb9aa283beab7
westulleken <78081982+westulleken@users.noreply.github.com>
2025-11-29 09:55:37 +0200

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from flask_cors import CORS
+
 from routes.patient_routes import patient_bp
+from routes.dropdown_routes import dropdown_bp
 
 app = Flask(__name__)
 CORS(app)
 
+# Register blueprints
 app.register_blueprint(patient_bp, url_prefix="/api")
+app.register_blueprint(dropdown_bp, url_prefix="/api")
 
 @app.route("/")
 def home():
