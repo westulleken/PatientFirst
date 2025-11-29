285cb192586befca0d9c772b68e9f02507e55b0f
westulleken <78081982+westulleken@users.noreply.github.com>
2025-11-29 09:59:41 +0200

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,7 @@
+@app.route("/admin/dropdowns")
+def admin_dropdowns():
+    return render_template("admin_dropdowns.html")
+from flask import render_template
 from flask import Flask
 from flask_cors import CORS
 
